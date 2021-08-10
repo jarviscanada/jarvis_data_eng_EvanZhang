e3d24e3d481617965d79d12f2bc4a94041a5cbcf
Evan <esz.zhang@gmail.com>
2021-08-10 23:11:34 +0000

    JDBC Orders DAO imp

--- core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/jdbc/JDBCExecutor.java
@@ -1,9 +1,24 @@
 package ca.jrvs.apps.jdbc;
 
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class JDBCExecutor {
 
   public static void main(String[] args) {
-    System.out.println("Learning");
+    DatabaseConnectionManager dcm = new DatabaseConnectionManager("localhost",
+        "hplussport", "postgres", "password");
+    try {
+      Connection connection= dcm.getConnection();
+      OrderDAO orderDAO = new OrderDAO(connection);
+
+      Order order = orderDAO.findById(1060);
+      System.out.println(order);
+    } catch (SQLException ex) {
+      ex.printStackTrace();
+    }
 
   }
 }
